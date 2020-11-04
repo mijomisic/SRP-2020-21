b1d50a06f7833a10672eeea40f22afd940dce375
Mijo <mijomisic50@gmail.com>
2020-11-04 11:06:40 +0100

    Implement initial login

--- postgres-express-node/api/index.js
+++ postgres-express-node/api/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const hello = require("./routes/hello");
 const user = require("./routes/user");
+const login = require("./routes/login");
 
 module.exports = () => {
   hello(router);
+  login(router);
   user(router);
 
   return router;
--- postgres-express-node/services/index.js
+++ postgres-express-node/services/index.js
@@ -1,7 +1,9 @@
 const winston = require("winston");
+const LoginService = require("./login.service");
 const UserService = require("./user.service");
 const { User } = require("../models");
 
 const logger = winston.loggers.get("logger");
 
-exports.userServiceInstance = new UserService({ logger, userModel: User });
+exports.loginServiceInstance = new LoginService({ logger, userModel: User });
+exports.userServiceInstance = new UserService({ logger, userModel: User })
