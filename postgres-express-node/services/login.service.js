8e2de485450e9d3ab682ca7ab09a3835292de339
Mijo <mijomisic50@gmail.com>
2020-11-11 11:15:57 +0100

    Refecator login to issue true JWT tokens

--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,3 +1,8 @@
+
+const jwt =require("jsonwebtoken");
+const config = require("../config");
+
+
 class LoginService {
   constructor({ logger, userModel }) {
     this.userModel = userModel;
@@ -9,6 +14,43 @@ class LoginService {
       where: userDTO,
     });
     return user;
+  }
+
+  async login({username,password}){
+    const userRecord = await this.userModel.findOne({
+      where: {username},
+    });
+
+    if(!userRecord){
+      this.logger.error("User not registered");
+      throw new Error("Authentication failed");
+    }
+
+    this.logger.info("Checking password");
+    if (userRecord.password === password){
+      this.logger.info("Password correct");
+      
+      const user = {
+        username: userRecord.username,
+        role: userRecord.role || "guest",
+      };
+
+      const payload = {
+        ...user,
+        aud: config.jwt.audience || "localhost/api",
+        iss: config.jwt.issuer || "localhost@fesb",
+      };
+
+      const token = this.generateToken(payload);
+
+      return {user, token};
+    }
+  }
+
+  generateToken(payload){
+    return jwt.sign(payload, config.jwt.secret, {
+      expiresIn: config.jwt.expiresIn,
+    });
   }
 }
 
